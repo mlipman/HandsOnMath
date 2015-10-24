caa057a8d2e51bb85f4f5cbf7134b979201ad1eb
Michael Lipman <michael.b.lipman@gmail.com>
2015-10-24 05:18:01 -0400

    Initial Commit

---
+++ HandsOnMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandsOnMath
+//
+//  Created by Michael Lipman on 10/24/15.
+//  Copyright (c) 2015 HackingEDUGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
