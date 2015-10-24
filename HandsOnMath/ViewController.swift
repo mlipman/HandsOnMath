5fe85f9353315534bdb1b3fb7dd70ba817babe2b
Michael Lipman <michael.b.lipman@gmail.com>
2015-10-24 06:27:14 -0400

    tap to expand, pinch to contract

--- HandsOnMath/ViewController.swift
+++ HandsOnMath/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var fiveLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tappedFive(sender: UITapGestureRecognizer) {
+        if (sender.state == .Ended) {
+            sender.view!.hidden = true
+            let repeatText = "x"
+            for _ in 1..<5 {
+                xLabel.text = (xLabel.text ?? "") + "•" + repeatText
+            }
+        }
     }
-
+    @IBAction func didPinchX(sender: UITapGestureRecognizer) {
+        if (sender.state == .Ended) {
+            let x = sender.view as! UILabel
+            if (x.text! == "x•x•x•x•x") {
+                x.text = "x"
+                fiveLabel.hidden = false
+            }
+        }
+    }
 
 }
 
