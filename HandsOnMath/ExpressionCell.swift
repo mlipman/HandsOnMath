f93cc9bc3a42f0dc3e64c1147a975d47296a1fd2
Michael Lipman <michael.b.lipman@gmail.com>
2016-01-26 09:23:10 +0200

    skeletal pieces of master detail view

---
+++ HandsOnMath/ExpressionCell.swift
@@ -0,0 +1,25 @@
+//
+//  ExpressionCell.swift
+//  HandsOnMath
+//
+//  Created by Michael Lipman on 1/25/16.
+//  Copyright © 2016 HackingEDUGroup. All rights reserved.
+//
+
+import UIKit
+
+class ExpressionCell: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
